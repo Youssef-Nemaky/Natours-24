694d7834ce1fa58b2850efe322673415cc63f117
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-10-30 13:22:23 +0300

    Refactoring to use usersRouter

--- app.js
+++ app.js
@@ -4,49 +4,14 @@ const express = require('express');
 const morgan = require('morgan');
 
 const toursRouter = require('./routes/tourRoutes');
+const usersRouter = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1/tours', toursRouter);
-
-const getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'failed', message: 'Route is not yet implemented' });
-};
-
-const getUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'failed', message: 'Route is not yet implemented' });
-};
-
-const createUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'failed', message: 'Route is not yet implemented' });
-};
-
-const updateUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'failed', message: 'Route is not yet implemented' });
-};
-
-const deleteUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'failed', message: 'Route is not yet implemented' });
-};
-
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/users', usersRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
