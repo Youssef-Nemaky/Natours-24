106186e406abc05eeb1b0e0719324602f75f75a9
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-10-30 13:29:21 +0300

    Refactoring to use userControoer

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+exports.getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'failed', message: 'Route is not yet implemented' });
+};
+
+exports.getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'failed', message: 'Route is not yet implemented' });
+};
+
+exports.createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'failed', message: 'Route is not yet implemented' });
+};
+
+exports.updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'failed', message: 'Route is not yet implemented' });
+};
+
+exports.deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'failed', message: 'Route is not yet implemented' });
+};
