7e47b5274d38f53ef08393d8f7fe9f0bb2c7d891
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-11-01 18:04:41 +0200

    Adding MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+mongoose
+  .connect(process.env.MONGO_URi)
+  .then(console.log('Connected to DB...'))
+  .catch((err) => console.log('Failed to connect to db', err));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
