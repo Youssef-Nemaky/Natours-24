96f2048c71b6f62d5d8b7f2d03b0572d20e00562
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-11-01 20:11:15 +0200

    Add getAllTours controller and fix a typo for status message

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,15 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
+exports.getAllTours = async (req, res) => {
   // const tours = fs.createReadStream('./dev-data/data/tours-simple.json', 'utf8');
   // data.pipe(tours);
-  //   res.status(200).json(tours);
+
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({ status: 'success', length: tours.length, tours });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: err });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -12,7 +18,7 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body);
     res.status(201).json({ status: 'success', data: newTour });
   } catch (err) {
-    res.status(400).json({ status: 'success', message: err });
+    res.status(400).json({ status: 'fail', message: err });
   }
 };
 
