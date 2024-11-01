4ea0876cbf17fdb7497092e26cefd0df88a3c58b
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-11-01 22:43:15 +0200

    Add code for getTour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,12 +18,17 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body);
     res.status(201).json({ status: 'success', data: newTour });
   } catch (err) {
-    res.status(400).json({ status: 'fail', message: err });
+    res.status(404).json({ status: 'fail', message: err });
   }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({ status: 'Sucess', data: req.tour });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({ status: 'success', tour });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
 };
 
 exports.updateTour = (req, res) => {
