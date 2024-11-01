c65571388cfa55c03c7cf3efc523b776cd21d4be
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-11-01 18:10:27 +0200

    Add tourModel.js

---
+++ models/tourModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    required: [true, 'Tour name is required'],
+    unique: true,
+    type: Number,
+  },
+  price: { required: [true, 'Tour price is required'], type: Number },
+  rating: { type: Number, default: 4.5 },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
