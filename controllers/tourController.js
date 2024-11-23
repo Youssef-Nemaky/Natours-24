4274bd209a84dc7e551814deb38dbffa5d68105b
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-11-23 22:29:34 +0200

    Add fields for top-5-tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -98,5 +98,6 @@ exports.aliasTopTours = (req, res, next) => {
   // limit=5&sort=-ratingsAverage,price
   req.query.limit = 5;
   req.query.sort = '-ratingsAverage price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
