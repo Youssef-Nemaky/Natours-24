45c5de25fec544707c4d2ada6d3ab8ab6e3932a1
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-12-07 15:32:56 +0200

    Add query middleware to stop showing secret tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -74,6 +74,10 @@ const tourSchema = new mongoose.Schema(
     },
 
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -88,6 +92,11 @@ tourSchema.virtual('durationWeeks').get(function () {
 
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
   next();
 });
 
