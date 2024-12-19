221330bde8ebf7490232a7f01db08c1099de6ecf
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2024-12-19 20:18:09 +0200

    Add global error handling middleware

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ app.use(morgan('dev'));
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  const error = new Error(`Can't find ${req.originalUrl} on this server`);
+  error.statusCode = 404;
+  error.status = 'fail';
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
