cbf9e3ea7a2db0951fb510e4c907c8c5aff4cf98
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-01-04 16:07:54 +0200

    Add login routing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,13 @@ const {
   deleteUser,
 } = require('../controllers/userController');
 
-const { signup } = require('../controllers/authController');
+const { signup, login } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 router.route('/signup').post(signup);
+router.route('/login').post(login);
 
 module.exports = router;
