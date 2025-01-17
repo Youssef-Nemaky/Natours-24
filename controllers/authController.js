609a0a50541732faade78cd977873035b1262a28
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-01-17 19:11:29 +0200

    feat: send JWT in httpOnly cookie feature but not yet used

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,30 @@ const createToken = (id) => {
   return token;
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = createToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: true,
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
