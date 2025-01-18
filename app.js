5ab8c7add8a19543b0a9529ba39cec8dbbb86f3f
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-01-18 03:10:13 +0200

    feat: add hpp for http parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { rateLimit } = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
@@ -18,9 +19,23 @@ app.use(express.json({ limit: '10kb' }));
 // set security headers
 app.use(helmet());
 
-// sanitize data to prevent NoSQL query injection
+// sanitize data against NoSQL query injection
 app.use(mongoSanitize());
+// sanitize data against xss (cross-site scipt)
 app.use(xss());
+// sanitize data aginst hpp (http parameter polution)
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 const appLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
